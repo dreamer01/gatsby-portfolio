0ff4d4850c0b5f05a861ab161f36874833bf5ec9
Pushpendra Singh <psnitsri01@gmail.com>
2018-12-26 17:47:59 +0530

    Images

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -158,9 +158,9 @@ const Index = () => (
       <Content speed={0.4} offset={0}>
         <Hero>
           <BigTitle>
-            Hello, <br /> I'm John Doe.
+            Hello, <br /> I'm Pushpendra Singh.
           </BigTitle>
-          <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+          <Subtitle>I'm creating web experiences for the next generation of consumer-facing companies.</Subtitle>
         </Hero>
       </Content>
       <DividerMiddle
