00185dc155b6dae29c17749e96922f15b6bd0784
Pushpendra Singh <psnitsri01@gmail.com>
2019-01-25 13:54:02 +0530

    Adding small changes

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,47 +1,60 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-import config from '../../config/website';
+import React from "react";
+import Helmet from "react-helmet";
+import config from "../../config/website";
 
 const SEO = () => {
-  const title = config.siteTitle;
-  const description = config.siteDescription;
-  const realPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
-  const image = config.siteUrl + realPrefix + config.siteLogo;
-  const blogURL = config.siteUrl + config.pathPrefix;
-  const schemaOrgJSONLD = [
-    {
-      '@context': 'http://schema.org',
-      '@type': 'WebSite',
-      url: blogURL,
-      name: title,
-      alternateName: config.siteTitleAlt ? config.siteTitleAlt : '',
-    },
-  ];
-  return (
-    <Helmet>
-      <html lang={config.siteLanguage} />
-      <title>{title}</title>
-      <link rel="apple-icon" href="/favicons/apple-icon.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
-      <link rel="shortcut icon" href="favicon.ico" />
-      <meta name="msapplication-TileColor" content={config.backgroundColor} />
-      <meta name="msapplication-config" content="browserconfig.xml" />
-      <meta name="description" content={description} />
-      <meta name="image" content={image} />
-      <script type="application/ld+json">{JSON.stringify(schemaOrgJSONLD)}</script>
-      <meta property="og:locale" content={config.ogLanguage} />
-      <meta property="og:site_name" content={config.ogSiteName} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-      <meta property="fb:app_id" content={config.siteFBAppID ? config.siteFBAppID : ''} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content={config.userTwitter ? config.userTwitter : ''} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
-    </Helmet>
-  );
+	const title = config.siteTitle;
+	const description = config.siteDescription;
+	const realPrefix = config.pathPrefix === "/" ? "" : config.pathPrefix;
+	const image = config.siteUrl + realPrefix + config.siteLogo;
+	const blogURL = config.siteUrl + config.pathPrefix;
+	const schemaOrgJSONLD = [
+		{
+			"@context": "http://schema.org",
+			"@type": "WebSite",
+			url: blogURL,
+			name: title,
+			alternateName: config.siteTitleAlt ? config.siteTitleAlt : ""
+		}
+	];
+	return (
+		<Helmet>
+			<html lang={config.siteLanguage} />
+			<title>{title}</title>
+			<link rel="apple-icon" href="/favicons/apple-icon.png" />
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="96x96"
+				href="/favicons/favicon-96x96.png"
+			/>
+			<link rel="shortcut icon" href="favicon.ico" />
+			<meta name="msapplication-TileColor" content={config.backgroundColor} />
+			<meta name="msapplication-config" content="browserconfig.xml" />
+			<meta name="description" content={description} />
+			<meta name="image" content={image} />
+			<script type="application/ld+json">
+				{JSON.stringify(schemaOrgJSONLD)}
+			</script>
+			<meta property="og:locale" content={config.ogLanguage} />
+			<meta property="og:site_name" content={config.ogSiteName} />
+			<meta property="og:title" content={title} />
+			<meta property="og:description" content={description} />
+			<meta property="og:image" content={image} />
+			<meta
+				property="fb:app_id"
+				content={config.siteFBAppID ? config.siteFBAppID : ""}
+			/>
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta
+				name="twitter:creator"
+				content={config.userTwitter ? config.userTwitter : ""}
+			/>
+			<meta name="twitter:title" content={title} />
+			<meta name="twitter:description" content={description} />
+			<meta name="twitter:image" content={image} />
+		</Helmet>
+	);
 };
 
 export default SEO;
